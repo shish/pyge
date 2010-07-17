ade9bd5fa8e1dabdabddca4d8c6b20bfe9eb3c08
Shish <shish@shishnet.org>
2010-07-17 12:25:51 +0100

    Time Stripper Mako-chan

---
+++ plugins/Mako.py
@@ -0,0 +1,31 @@
+from pygelib import PygePlugin
+import struct
+
+#
+# no sig or extension as found in Time Stripper Mako-chan
+#
+class Mako(PygePlugin):
+    name = "Mako"
+    desc = "Time Stripper Mako-chan"
+    sig = ""
+    header_fmt = "<h"
+    entry_fmt = "<12si"
+
+    def _readheader(self):
+        self.header_len = struct.unpack(self.header_fmt,
+                self.file.read(struct.calcsize(self.header_fmt)))[0]
+        self.count = (self.header_len - 2) / struct.calcsize(self.entry_fmt)
+
+    def _readindex(self):
+        prev = None
+        for n in xrange(self.count):
+            name, offset = struct.unpack(self.entry_fmt,
+                    self.file.read(struct.calcsize(self.entry_fmt)))
+            name = name.strip("\0")
+            self.list[name] = {"name":name, "start":offset, "length":0}
+            if prev:
+                self.list[prev]["length"] = offset - self.list[prev]["start"]
+            prev = name
+        self.file.seek(0, 2)
+        self.list[prev]["length"] = self.file.tell() - self.list[prev]["start"]
+
